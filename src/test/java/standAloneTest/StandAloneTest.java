38839dce83a5a44e65e36d444ded97006c50a318
shimaa4455 <shimaa.shehata6@yahoo.com>
2026-01-12 19:19:06 +0200

    finalize the e2e test case

--- src/test/java/standAloneTest/StandAloneTest.java
+++ src/test/java/standAloneTest/StandAloneTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -14,12 +15,19 @@ import java.util.List;
 public class StandAloneTest {
 
     public static void main(String[] args) {
+        //=========test data===============================
+        String countryName = "Albania";
+        String productName = "ADIDAS ORIGINAL";
+        String email ="shimaa@gmail.com";
+        String password="swaNy4455";
+        //============================E2E test case==================
+        //validate the idem added to the cart successfully.
         WebDriver driver = new ChromeDriver();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        String productName = "ZARA COAT 3";
+        driver.manage().window().maximize();
         driver.get("https://rahulshettyacademy.com/client");
-        driver.findElement(By.id("userEmail")).sendKeys("shimaa@gmail.com");
-        driver.findElement(By.id("userPassword")).sendKeys("swaNy4455");
+        driver.findElement(By.id("userEmail")).sendKeys(email);
+        driver.findElement(By.id("userPassword")).sendKeys(password);
         driver.findElement(By.id("login")).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));
         List <WebElement> products= driver.findElements(By.cssSelector(".mb-3"));
@@ -36,5 +44,19 @@ public class StandAloneTest {
         boolean match = cartProducts.stream().anyMatch(product-> product.getText().equalsIgnoreCase(productName));
         System.out.println(match);
         Assert.assertTrue(match);
+        driver.findElement(By.cssSelector(".totalRow button")).click();
+        Actions actions = new Actions(driver);
+        actions.sendKeys(driver.findElement(By.xpath("//input[@placeholder='Select Country']")),countryName).build().perform();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ta-results")));
+        //driver.findElement(By.xpath("//input[@placeholder='Select Country']/following-sibling::section//button/span[normalize-space(text())='India']")).click();
+        String xpath = String.format("//input[@placeholder='Select Country']/following-sibling::section//button/span[normalize-space(text())='%s']", countryName);
+        driver.findElement(By.xpath(xpath)).click();
+        driver.findElement(By.cssSelector(".action__submit")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".hero-primary")));
+        String message= driver.findElement(By.cssSelector(".hero-primary")).getText();
+        System.out.println(message);
+        Assert.assertTrue(message.equalsIgnoreCase("Thankyou for the order."));
+
+
     }
 }
